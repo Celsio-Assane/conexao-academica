6b0f9883fc920d158cda2ef1ad73de7f90ded211
Célsio Assane <67740598+Celsio-Assane@users.noreply.github.com>
2025-04-22 22:19:05 -0300

    Atualizei o texto de apresentação do site

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+  window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY;
+    const docHeight = document.body.scrollHeight - window.innerHeight;
+    const scrollPercent = (scrollTop / docHeight) * 100;
+
+    document.getElementById('progress-bar').style.width = scrollPercent + '%';
+  });
+</script>
