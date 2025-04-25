8b85e33f4692b77b60da53adc3cafac71018bd08
Célsio Assane <67740598+Celsio-Assane@users.noreply.github.com>
2025-04-24 21:22:27 -0300

    Houve alterations

--- script.js
+++
@@ -1,9 +0,0 @@
-<script>
-  window.addEventListener('scroll', () => {
-    const scrollTop = window.scrollY;
-    const docHeight = document.body.scrollHeight - window.innerHeight;
-    const scrollPercent = (scrollTop / docHeight) * 100;
-
-    document.getElementById('progress-bar').style.width = scrollPercent + '%';
-  });
-</script>
